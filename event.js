1ba759e25477df9c47f81a0de3b8ce3f8ef0be40
Vivek Agarwal <nitkkr.vivek@gmail.com>
2013-04-24 02:49:13 +0530

    adding id

--- event.js
+++ event.js
@@ -1,4 +1,4 @@
-var Event = function(title, description, participants, startTime, endTime) {
+var Event = function(title, description, participants, startTime, endTime, eventId) {
   this.title = title;
   this.description = description;
   this.participants = participants;
@@ -6,4 +6,7 @@ var Event = function(title, description, participants, startTime, endTime) {
   this.startTime = startTime;
   // This is in string format as input, need to fix.
   this.endTime = endTime;
+
+  // Event id, not filled yet.
+  this.eventId = eventId;
 };
